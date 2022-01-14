7b9e5dcd2a286ba0b4bd682f2d9fa12038061788
Amogus <erlan.chkalov007@gmail.com>
2022-01-14 21:21:29 +0600

    HW1

---
+++ Practice1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = DateTime.Now.ToShortDateString();
+            string name = "Erlan";
+            Console.WriteLine($"Hello, {name}, today is {time}");
+        }
+    }
+}
