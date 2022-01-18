708121550de3290a23e0f5bec93c312749ccdb03
Amogus <erlan.chkalov007@gmail.com>
2022-01-18 19:29:16 +0600

    HW3

---
+++ Practice2/Practice2_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practice2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Petrov";
+            int receipt = 3287;
+            int total = 52210;
+            Console.WriteLine(" _____________________________________");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|           OOO ChekNaZakaz           |");
+            Console.WriteLine($"|              Чек № {receipt}             |");
+            Console.WriteLine($"|            Кассир: {name}           |");
+            Console.WriteLine("| 1. Брус 18х18 7,2 куб.м. х 2700     |");
+            Console.WriteLine("| 2. Доска обрезная сосна 11,3 куб. м.|");
+            Console.WriteLine("|    х 2900                           |");
+            Console.WriteLine("|=====================================|");
+            Console.WriteLine($"|Всего...........................{total}|");
+            Console.WriteLine("|ККМ 11111111 ИНН 222222222222  № 3287|");
+            Console.WriteLine("|12.06.08 11:43                 Петров|");
+            Console.WriteLine("|ПРОДАЖА                         №3287|");
+            Console.WriteLine("|1                           =52210.00|");
+            Console.WriteLine("|ИТОГ     =52210.00                   |");
+            Console.WriteLine($"| НАЛИЧНЫМИ                     ={total}|");
+            Console.WriteLine("||||||||||||||||||ФП|||||||||||||||||||");
+            Console.WriteLine("|                     ЭКЛЗ 3333333333 |");
+            Console.WriteLine("|                     00061528 #025843|");
+            Console.WriteLine("|_____________________________________|");
+        }
+    }
+}
---
+++ Practice2/Practice2_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Practice2_5
+{
+    class Program
+    {
+        enum Months
+        {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter max temp for a day: ");
+            float maxt = float.Parse(Console.ReadLine());
+            Console.Write("Enter min temp for a day: ");
+            float mint = float.Parse(Console.ReadLine());
+            float avg = (maxt + mint) / 2;
+            Console.WriteLine($"Average temp for a day is {avg}");
+            Console.Write("Enter the number of month: ");
+            int month = int.Parse(Console.ReadLine());
+            Months typed_month = (Months)month;
+            if (month <= 12 && month > 0)
+            {
+                if (avg > 0 && month <= 2)
+                    Console.WriteLine("Rainy winter");
+                else if (avg > 0 && month == 12)
+                    Console.WriteLine("Rainy winter");
+                else
+                    Console.WriteLine($"You have entered {typed_month.ToString()}");
+                return;
+            }
+            Console.WriteLine("There is no such a month");
+        }
+    }
+}
